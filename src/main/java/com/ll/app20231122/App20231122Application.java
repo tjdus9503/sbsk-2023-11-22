dc06ded4e081dbbd805ef7d61b684f56e53b8d69
tjdus9503 <tjdus03095@gmail.com>
2023-11-22 14:06:28 +0900

    setting

---
+++ src/main/java/com/ll/app20231122/App20231122Application.java
@@ -0,0 +1,13 @@
+package com.ll.app20231122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20231122Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App20231122Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/app20231122/App20231122ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.app20231122;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20231122ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
